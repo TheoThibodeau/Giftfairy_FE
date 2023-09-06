8aaa25a202304349ea90983d596196e949c10eee
Laura-D-Andrews <ldandrews88@gmail.com>
2023-09-06 15:43:08 -0400

    added buttons to filters

--- Bestow-FE/src/components/filters.jsx
+++ Bestow-FE/src/components/filters.jsx
@@ -25,20 +25,56 @@ const Filters = () => {
             .then (() => {
 
             })
+            console.log(handlePost)
+    }
+
+    const handleAge = (selectedAge) => {
+        setAge(selectedAge)
+        console.log(selectedAge)
+    }
+
+    const handleGender = (selectedGender) => {
+        setGender(selectedGender)
+        console.log(selectedGender)
+    }
+
+    const handleRelationship = (selectedRelationship) => {
+        setRelationship(selectedRelationship)
+        console.log(selectedRelationship)
+    }
+
+    const handlePriceRange = (selectedPriceRange) => {
+        setPriceRange(selectedPriceRange)
+        console.log(selectedPriceRange)
+    }
+
+    const handleOccasion = (selectedOccasion) => {
+        setOccasion(selectedOccasion)
+        console.log(selectedOccasion)
+    }   
+
+    const handleGiftType = (selectedGiftType) => {
+        setGiftType(selectedGiftType)
+        console.log(selectedGiftType)
+    }
+
+    const handleInterests = (selectedInterests) => {
+        setInterests(selectedInterests)
+        console.log(selectedInterests)
     }
 
     return (
         <>
             <h1>Bestow</h1>
             <p>Age</p>
-                <button>
+                <button onClick={() => handleAge("0-5")}>
                     <h5>0-5</h5>
                 </button>
                 <button>
                     <h5>6-12</h5>
                 </button>
                 <button>
-                    <h5>13-20-</h5>
+                    <h5>13-20</h5>
                 </button>
                 <button>
                     <h5>21-30</h5>
@@ -56,12 +92,30 @@ const Filters = () => {
                     <h5>65+</h5>
                 </button>
             <p>Gender</p>
+            <button onClick={() => handleGender("male")}>
+                    <h5>male</h5>
+                </button>
             <p>Relationship</p>
+            <button onClick={() => handleRelationship("partner")}>
+                    <h5>partner</h5>
+                </button>
             <p>Price Range</p>
+            <button onClick={() => handlePriceRange("$10-$15")}>
+                    <h5>$10-$15</h5>
+                </button>
             <p>Occasion</p>
+            <button onClick={() => handleOccasion("Christmas")}>
+                    <h5>Christmas</h5>
+                </button>
             <p>Gift Type</p>
+            <button onClick={() => handleGiftType("Product")}>
+                    <h5>Product</h5>
+                </button>
             <p>Interests</p>
-
+            <button onClick={() => handleInterests("Cooking")}>
+                    <h5>Cooking</h5>
+                </button>
+        <button onClick={handlePost}>Generate</button>
         </>
     )
 }; 
