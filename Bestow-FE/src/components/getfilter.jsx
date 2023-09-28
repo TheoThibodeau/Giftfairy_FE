7b2a22e7c6b27d81756f74fdff780bc9df298c26
TheoThibodeau <teddythibodeau@gmail.com>
2023-09-28 15:38:06 -0400

    state variables to receive output text

--- Bestow-FE/src/components/getfilter.jsx
+++ Bestow-FE/src/components/getfilter.jsx
@@ -3,11 +3,21 @@ import axios from "axios";
 
 const getFilterResponse = ({ }) => {
 
+    const [output, setOutput] = useState("");
+
+    useEffect(() => {
+        axios
+            .get('http://127.0.0.1:8000/api/filter/response')
+            .then((response) => {
+                setOutput(response.data.output);
+            })
+    })
+
     return(
         <>
-
+            <h3>{output}</h3>
         </>
     );
 };
 
-export default getFilterResponse
+export default getFilterResponse;
