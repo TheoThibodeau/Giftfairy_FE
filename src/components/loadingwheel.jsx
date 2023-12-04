0bfc581bc35b4c260cd6472e2ebdba908b622d22
kekedezha <dezhac6@gmail.com>
2023-12-04 14:40:27 -0500

    current project place and work for importing spinner for loading screen...not working.

--- src/components/loadingwheel.jsx
+++ src/components/loadingwheel.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Spinner from 'react-bootstrap/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function GrowExample() {
-    return (
-        <>
-
-            <Spinner variant="dark" animation="grow" />
-
-        </>
+    return  (
+    <>
+        <Spinner variant="dark" animation="grow" />
+    </>
     );
   }
   
