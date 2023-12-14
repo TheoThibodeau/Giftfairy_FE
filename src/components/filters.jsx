b0ed87a29741e8986bc356fe55cd72d7d13f7d54
kekedezha <dezhac6@gmail.com>
2023-12-14 17:50:44 -0500

    updated intro page sentence/word for typewriter effect to work

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -216,7 +216,7 @@ const Filters = () => {
 
     //Object containing messages for each selection page
     const promptMessages = {
-      intro: "Hey gift fairy here! Before I can give you some great gift ideas, I need to know a little bit more about the person you are shopping for.",
+      intro: "Heey gift fairy here! Before I can give you some great gift ideas, I need to know a little bit more about the person you are shopping for.",
       gender: "What is their gender?",
       age: "How old are they?", 
       relationship: "What is your relationship with them?", 
