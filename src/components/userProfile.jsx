bde155a653e5bc997509cdd84bd4502c92a814c1
TheoThibodeau <teddythibodeau@gmail.com>
2024-01-18 15:45:58 -0500

    dropdown menu for profile history

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -4,6 +4,8 @@ import dayjs from "dayjs";
 
 const GetProfileResponse = () => {
   const [profile, setProfile] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+
 
   useEffect(() => {
     axios
@@ -14,6 +16,10 @@ const GetProfileResponse = () => {
       });
   }, []);
 
+  const handleItemClick = (index) => {
+    setSelectedItem(selectedItem === index ? null : index);
+  };
+
   return (
     <>
       <h1>User Profile</h1>
@@ -21,18 +27,24 @@ const GetProfileResponse = () => {
       <div className="userProfileHistory">
         {profile.map((item, index) => (
             <div key={index}>
-                <h3>Date: {dayjs(item.created_at).format("MM-DD-YYYY hh:mm:ss A")}</h3>
-                <h3>Age: {item.age}</h3>
-                <h3>Gender: {item.gender}</h3>
-                <h3>Relationship: {item.relationship}</h3>
-                <h3>Price Range: {item.price_range}</h3>
-                <h3>Occasion: {item.occasion}</h3>
-                <h3>Gift Type: {item.gift_type}</h3>
-                <h3>Interest: {item.interest}</h3>
-                <h3>Activity: {item.activity_level}</h3>
-                <h3>Personality: {item.personality}</h3>
-                <h3>Nature: {item.nature}</h3>
-                <h3>Generated Gift Ideas: {item.item_title_string}</h3>
+                <h3 onClick={() => handleItemClick(index)}>
+                    Date: {dayjs(item.created_at).format("MM-DD-YYYY hh:mm:ss A")}
+                </h3>
+                {selectedItem === index && (
+                    <div className="details">
+                        <h3>Age: {item.age}</h3>
+                        <h3>Gender: {item.gender}</h3>
+                        <h3>Relationship: {item.relationship}</h3>
+                        <h3>Price Range: {item.price_range}</h3>
+                        <h3>Occasion: {item.occasion}</h3>
+                        <h3>Gift Type: {item.gift_type}</h3>
+                        <h3>Interest: {item.interest}</h3>
+                        <h3>Activity: {item.activity_level}</h3>
+                        <h3>Personality: {item.personality}</h3>
+                        <h3>Nature: {item.nature}</h3>
+                        <h3>Generated Gift Ideas: {item.item_title_string}</h3>
+                    </div>
+                )}
             </div>
         ))}
       </div>
