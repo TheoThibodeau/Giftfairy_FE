6a5f488b2793ef7536324f34e8aea62669c1f56d
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-15 18:05:38 -0500

    ui changes to the landing page
    Co-authored-by: Christian Dezha <dezhac6@gmail.com>

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,38 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import GiftfairyImage from '../images/giftfairy.png';
+import GiftfairyImage from "../images/giftfairy.png";
+import RightArrow from "../images/right-arrow.png";
 
 const Landing = () => {
-
-    const handleBegin = () => {
-        Navigate("/filters");
+  const handleBegin = () => {
+    Navigate("/filters");
   };
 
-
-    return(
-        <>
-            <h1>GIFTFaiRY</h1>
-            <div>
-                <img 
-                src={GiftfairyImage} 
-                alt="Gift Fairy"
-                height={200}
-                width={200}
-                 />
-            </div>
-            <h4 className="landingDescription">GIFTFaiRY selects the perfect gift for every 
-            personality with our AI-powered 
-            shopping companion – thoughtful, 
-            personalized, and just one click 
-            away!</h4>
-            <Link to="/selections">
-                <button className="landingButton">Let's Begin</button>
-            </Link>
-            <h6 className="version">
-                version 3.0.0
-            </h6>
-        </>
-    )
+  return (
+    <>
+      <h1>GIFTFaiRY</h1>
+      <div>
+        <img src={GiftfairyImage} alt="Gift Fairy" height={200} width={200} />
+      </div>
+      <div className="landingDescription">
+        <h2 className="oxygen-bold">Curated Gifts</h2>
+        <h2 className="oxygen-bold">made easy</h2>
+        <h2 className="oxygen-bold">through the power of AI</h2>
+      </div>
+      <Link to="/selections">
+        <button className="landingButton oxygen-bold">
+          Get Started
+          <img src={RightArrow} height={30} width={30} />
+        </button>
+      </Link>
+      <h6 className="version">version 3.0.0</h6>
+    </>
+  );
 };
 
 export default Landing;
