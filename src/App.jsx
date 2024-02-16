2d6457c1efe7c77ac34251d078e08f8d0ef88671
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-16 17:50:44 -0500

    restyled the landing page
    Co-authored-by: Christian Dezha <dezhac6@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,70 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Filters from './components/filters';
-import Landing from './components/landing';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Filters from "./components/filters";
+import Landing from "./components/landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GetProfileResponse from './components/userProfile';
+import GetProfileResponse from "./components/userProfile";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 function App() {
   const [count, setCount] = useState(0);
-  const [token, setToken] = useState('');
-  const [user, setUser] = useState('');
-  const [authentication, setAuthentication] = useState('');
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState("");
+  const [authentication, setAuthentication] = useState("");
 
   const handleUserLogin = (newUser) => {
     setUser(newUser);
   };
 
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyD31E0qyRff38YbacqfmJX1mLePHxanrws",
-      authDomain: "giftfairy-a84c0.firebaseapp.com",
-      projectId: "giftfairy-a84c0",
-      storageBucket: "giftfairy-a84c0.appspot.com",
-      messagingSenderId: "360717671227",
-      appId: "1:360717671227:web:1a35bb12a01c2799f7f51e",
-      measurementId: "G-TDHVENEFD9"
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD31E0qyRff38YbacqfmJX1mLePHxanrws",
+    authDomain: "giftfairy-a84c0.firebaseapp.com",
+    projectId: "giftfairy-a84c0",
+    storageBucket: "giftfairy-a84c0.appspot.com",
+    messagingSenderId: "360717671227",
+    appId: "1:360717671227:web:1a35bb12a01c2799f7f51e",
+    measurementId: "G-TDHVENEFD9",
   };
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-    // Initialize Firebase Authentication and get a reference to the service for the giftfairy app
-    const auth = getAuth(app);
+  // Initialize Firebase Authentication and get a reference to the service for the giftfairy app
+  const auth = getAuth(app);
 
-    useEffect(() => {
-      setAuthentication(auth);
-    }, [auth]);
+  useEffect(() => {
+    setAuthentication(auth);
+  }, [auth]);
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/selections" element={<Filters handleUserLogin={handleUserLogin} authentication={authentication} />}></Route>
-      <Route path="/profile" element={<GetProfileResponse handleUserLogin={handleUserLogin} authentication={authentication}/>}></Route>
-    </Routes> 
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/selections"
+            element={
+              <Filters
+                handleUserLogin={handleUserLogin}
+                authentication={authentication}
+              />
+            }
+          ></Route>
+          <Route
+            path="/profile"
+            element={
+              <GetProfileResponse
+                handleUserLogin={handleUserLogin}
+                authentication={authentication}
+              />
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-};
+  );
+}
 
 export default App;
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import GiftfairyImage from "../images/giftfairy.png";
+import GiftfairyImage from "../images/giftfairymaybe.jpeg";
 import RightArrow from "../images/right-arrow.png";
 
 const Landing = () => {
@@ -10,14 +10,25 @@ const Landing = () => {
 
   return (
     <>
-      <h1>GIFTFaiRY</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1240 310">
+    <path
+        fill="#ffa517"
+        fill-opacity="1"
+        d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+    ></path>
+    </svg>
+      <h1 className="landingTitle">
+        giftf<i>ai</i>ry
+      </h1>
       <div>
         <img src={GiftfairyImage} alt="Gift Fairy" height={200} width={200} />
       </div>
       <div className="landingDescription">
-        <h2 className="oxygen-bold">Curated Gifts</h2>
+        <h2 className="oxygen-bold">curated gifts</h2>
         <h2 className="oxygen-bold">made easy</h2>
-        <h2 className="oxygen-bold">through the power of AI</h2>
+        <h2 className="oxygen-bold">
+          through the power of <i>ai</i>
+        </h2>
       </div>
       <Link to="/selections">
         <button className="landingButton oxygen-bold">
@@ -25,7 +36,7 @@ const Landing = () => {
           <img src={RightArrow} height={30} width={30} />
         </button>
       </Link>
-      <h6 className="version">version 3.0.0</h6>
+      <h6 className="version">version 4.0.0</h6>
     </>
   );
 };
