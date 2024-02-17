b061606670b1eea4b5af10e0e171cfa4245fe99c
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-17 13:51:42 -0500

    css changes

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -6,12 +6,10 @@ import NavBar from "./navbar";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import { RingLoader } from "react-spinners";
 import TypeWriter from "./typewriter";
-import UserAuthentication from './userAuthentication';
+import UserAuthentication from "./userAuthentication";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-
 const Filters = ({ handleUserLogin, authentication }) => {
-
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
   const [relationship, setRelationship] = useState("");
@@ -36,19 +34,19 @@ const Filters = ({ handleUserLogin, authentication }) => {
   const [hasSelections, setHasSelections] = useState(false);
   const [authCurrentUser, setAuthCurrentUser] = useState(null);
   const [userEmail, setUserEmail] = useState("");
-  
+
   const handlePost = () => {
     setIsLoading(true);
     setIsGenerated(false);
-   axios
+    axios
       .post("https://giftfairy-be-server.onrender.com/api/filter/generate", {
         age: age,
         gender: gender,
         relationship: relationship,
         price_range: priceRange,
         occasion: occasion,
-        gift_type: giftType.join(', '),
-        interest: interests.join(', '),
+        gift_type: giftType.join(", "),
+        interest: interests.join(", "),
         activity_level: activity,
         personality: personality,
         nature: nature,
@@ -75,27 +73,34 @@ const Filters = ({ handleUserLogin, authentication }) => {
         //Set up variable to hold next prompt message depending on next active element
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
         if (error.response) {
           // The request was made and the server responded with a status code
-          console.error('Server responded with status code:', error.response.data);
+          console.error(
+            "Server responded with status code:",
+            error.response.data
+          );
           if (error.response.status == 400) {
-            alert("You left one or more of your choices blank please go back and redo your selections!")
+            alert(
+              "You left one or more of your choices blank please go back and redo your selections!"
+            );
           } else if (error.response.status == 404) {
-            alert("Back-End server endpoint not found. Server might have been discontinued.")
+            alert(
+              "Back-End server endpoint not found. Server might have been discontinued."
+            );
           } else if (error.response.status == 500) {
-            alert("Internal Server Error, please try again!")
+            alert("Internal Server Error, please try again!");
           } else if (error.response.status == 502) {
-            alert("Bad Gateway, please try again!")
+            alert("Bad Gateway, please try again!");
           } else if (error.response.status == 504) {
-            alert("Gateway Timeout, please try again!")
+            alert("Gateway Timeout, please try again!");
           }
         } else if (error.request) {
           // The request was made but no response was received
           alert("No response received from server, please try again!");
         } else {
           // Something else happened while setting up the request
-          alert('Error:', error.message);
+          alert("Error:", error.message);
         }
       });
   };
@@ -190,7 +195,8 @@ const Filters = ({ handleUserLogin, authentication }) => {
 
   //Object containing messages for each selection page
   const promptMessages = {
-    gifteeName: "Before we get started, I'm curious, what is your giftee's name?",
+    gifteeName:
+      "Before we get started, I'm curious, what is your giftee's name?",
     gender: "What is their gender?",
     age: "How old are they?",
     relationship: "What is your relationship with them?",
@@ -310,34 +316,34 @@ const Filters = ({ handleUserLogin, authentication }) => {
     console.log(selectionMade);
   };
 
-    //Filters.jsx - User Authentication Observer
-    //The observer tracks the user authentication token across the different components
-      
-      console.log("Auth Current User is: " + authCurrentUser);
+  //Filters.jsx - User Authentication Observer
+  //The observer tracks the user authentication token across the different components
 
-      useEffect(() => {
-        const auth = getAuth();
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-              // User is signed in
-              setAuthCurrentUser(user);
-              console.log("User is: " + user);
-              setUserEmail(user.email);
-              handleUserLogin(user); // Call the handler to update user state in parent component
-            } else {
-              // No user is signed in
-              setAuthCurrentUser(null);
-            }
-          });
-      
-        return () => unsubscribe(); // Cleanup function to unsubscribe from the observer
-      }, []);
+  console.log("Auth Current User is: " + authCurrentUser);
 
-    console.log(authCurrentUser);
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        setAuthCurrentUser(user);
+        console.log("User is: " + user);
+        setUserEmail(user.email);
+        handleUserLogin(user); // Call the handler to update user state in parent component
+      } else {
+        // No user is signed in
+        setAuthCurrentUser(null);
+      }
+    });
+
+    return () => unsubscribe(); // Cleanup function to unsubscribe from the observer
+  }, []);
+
+  console.log(authCurrentUser);
 
   return (
     <>
-      <div className="navbar-ProgressBar-Container">
+      {/* <div className="navbar-ProgressBar-Container">
         <NavBar />
         <ProgressBar
           now={progressValues[activeElement]}
@@ -351,17 +357,25 @@ const Filters = ({ handleUserLogin, authentication }) => {
             style={{ background: "whitesmoke", height: "24px" }}
           />
         </ProgressBar>
-      </div>
+      </div> */}
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1240 310">
+        <path
+          fill="#ffa517"
+          fill-opacity=".75"
+          d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+        ></path>
+      </svg>
 
       <br />
 
       {/* While going through selections and before making it to the generate page, show this 
       version of the "prompt-div" div */}
-      {(activeElement !== "generate") && (
+      {activeElement !== "generate" && (
         <div className="prompt-div">
           {/* Christian Dezha - 12/12/2023 */}
           <TypeWriter text={promptMessages[activeElement]} />
-          {(activeElement !== "generate" && activeElement !== "gifteeName") ? (
+          {activeElement !== "generate" && activeElement !== "gifteeName" ? (
             activeElement !== "giftType" && activeElement !== "interests" ? (
               <p>(Select One Option)</p>
             ) : (
@@ -373,7 +387,7 @@ const Filters = ({ handleUserLogin, authentication }) => {
         </div>
       )}
 
-      {(activeElement == "generate" && authCurrentUser && !isGenerated) && (
+      {activeElement == "generate" && authCurrentUser && !isGenerated && (
         <div className="prompt-div">
           {isLoading ? (
             <RingLoader color="#ffffff" />
@@ -391,7 +405,6 @@ const Filters = ({ handleUserLogin, authentication }) => {
           hasSelectionsHandler={setHasSelections}
         />
       </div>
-    
 
       {isGenerated && (
         <div className="container">
@@ -406,10 +419,8 @@ const Filters = ({ handleUserLogin, authentication }) => {
               <p>{itemDescrip[index]}</p>
               <a
                 key={index}
-                href={`https://www.amazon.com/s?k=${encodeURIComponent(
-                  title
-                )}`}
-                // &tag=giftfairy08-20 add amazon affiliate link once it has been approved. 
+                href={`https://www.amazon.com/s?k=${encodeURIComponent(title)}`}
+                // &tag=giftfairy08-20 add amazon affiliate link once it has been approved.
                 target="_blank"
                 rel="noreferrer"
               >
@@ -419,34 +430,43 @@ const Filters = ({ handleUserLogin, authentication }) => {
           ))}
           {isGenerated && (
             <div className="regenButton-div">
-              <button 
-              onClick={handlePost}
-              >
-                Re-Generate
-              </button>
+              <button onClick={handlePost}>Re-Generate</button>
             </div>
           )}
         </div>
       )}
 
-      {activeElement === "generate" && !isLoading && !isGenerated && authCurrentUser &&(
-        <button
-          onClick={handlePost}
-          disabled={isLoading}
-          className={`generateButton ${isLoading ? "opacity1" : "opacity2"}`}
-        >
-          Generate
-        </button>
-      )}
+      {activeElement === "generate" &&
+        !isLoading &&
+        !isGenerated &&
+        authCurrentUser && (
+          <button
+            onClick={handlePost}
+            disabled={isLoading}
+            className={`generateButton ${isLoading ? "opacity1" : "opacity2"}`}
+          >
+            Generate
+          </button>
+        )}
 
-      {activeElement === "generate" && !isLoading && !isGenerated && !authCurrentUser &&(
-        <>
-          <div className="prompt-div">
-          <TypeWriter text={"Before I can give you any suggestions, please login or register a new account!"} />
-          </div>
-          <UserAuthentication handleUserLogin={handleUserLogin} authentication={authentication}/>
-        </>
-      )}
+      {activeElement === "generate" &&
+        !isLoading &&
+        !isGenerated &&
+        !authCurrentUser && (
+          <>
+            <div className="prompt-div">
+              <TypeWriter
+                text={
+                  "Before I can give you any suggestions, please login or register a new account!"
+                }
+              />
+            </div>
+            <UserAuthentication
+              handleUserLogin={handleUserLogin}
+              authentication={authentication}
+            />
+          </>
+        )}
 
       <div className="footer">
         {!isGenerated && (
@@ -464,17 +484,16 @@ const Filters = ({ handleUserLogin, authentication }) => {
                 </button>
               )}
             </div>
-          
+
             {/* Next Button Logic for Single Selections */}
             <div className="nextButton-div">
-              {(activeElement == "age" 
-                || activeElement == "gender" 
-                || activeElement == "activity" 
-                || activeElement == "personality" 
-                || activeElement == "nature" 
-                || activeElement == "priceRange" 
-                || activeElement == "relationship") 
-                && (
+              {(activeElement == "age" ||
+                activeElement == "gender" ||
+                activeElement == "activity" ||
+                activeElement == "personality" ||
+                activeElement == "nature" ||
+                activeElement == "priceRange" ||
+                activeElement == "relationship") && (
                 <button
                   disabled={!selectionMade}
                   onClick={handleNextElement}
@@ -485,21 +504,22 @@ const Filters = ({ handleUserLogin, authentication }) => {
               )}
 
               {/* Next Button Logic for Multi Selections */}
-              {(activeElement == "giftType" 
-                || activeElement == "interests" 
-                || activeElement == "occasion") 
-                && (
+              {(activeElement == "giftType" ||
+                activeElement == "interests" ||
+                activeElement == "occasion") && (
                 <button
-                disabled={(!hasSelections || !selectionMade)}
-                onClick={handleNextElement}
-                className={`${hasSelections && selectionMade ? "opacity2" : "opacity1"}`}
-              >
-                Next
-              </button>
+                  disabled={!hasSelections || !selectionMade}
+                  onClick={handleNextElement}
+                  className={`${
+                    hasSelections && selectionMade ? "opacity2" : "opacity1"
+                  }`}
+                >
+                  Next
+                </button>
               )}
 
-            {/* Next button logic for 'Optional' gifteeName */}
-              {(activeElement == "gifteeName") && (
+              {/* Next button logic for 'Optional' gifteeName */}
+              {activeElement == "gifteeName" && (
                 <button
                   onClick={handleNextElement}
                   className={`${selectionMade}`}
