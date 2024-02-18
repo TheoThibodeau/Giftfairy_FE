2eda6912ee59b4908eb6e18dbbb60db0c38a4b55
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-18 17:36:51 -0500

    css changes

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -9,7 +9,8 @@ import TypeWriter from "./typewriter";
 import UserAuthentication from "./userAuthentication";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Link } from "react-router-dom";
-import UserImage from "../images/userImage.png";
+import UserImage from '../images/userImage.png';
+
 
 const Filters = ({ handleUserLogin, authentication }) => {
   const [age, setAge] = useState("");
@@ -362,12 +363,12 @@ const Filters = ({ handleUserLogin, authentication }) => {
       </div> */}
 
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1240 310">
+        <path
+          fill="#ffa517"
+          fillOpacity=".75"
+          d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+        ></path>
         <Link to="/profile">
-          <path
-            fill="#ffa517"
-            fillOpacity=".75"
-            d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
-          ></path>
           <img src={UserImage} alt="User Image" height={40} width={40} />
         </Link>
         <h2 className="titleHeader">GIFTFaiRY</h2>
