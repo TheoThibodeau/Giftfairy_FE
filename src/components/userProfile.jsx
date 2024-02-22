e49900dd65a88a7231ba34b976c1fa32f9d66363
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-22 17:13:01 -0500

    added users first name to profile page, added a ternary to display user name when logged in, and display a generic message when not logged in
    Co-Authored-by: Christian Dezha <dezhac6@gmail.com>

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -6,10 +6,11 @@ import UserAuthentication from "./userAuthentication";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const GetProfileResponse = ({ handleUserLogin, authentication }) => {
-  const [profile, setProfile] = useState([]);
+  const [userProfileHistory, setUserProfileHistory] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedItem, setSelectedItem] = useState(null);
   const [authCurrentUser, setAuthCurrentUser] = useState(null);
+  const [userFirstName, setUserFirstName] = useState([]);
 
   const handleDateClick = (index) => {
     setSelectedDate(selectedDate === index ? null : index);
@@ -34,29 +35,37 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
         handleUserLogin(user); // Call the handler to update user state in parent component
         axios
           .get(
+            `https://giftfairy-be-server.onrender.com/api/user/response/${user.email}/`
+          )
+          .then((response) => {
+            const items = response.data;
+            setUserFirstName(items);
+          });
+        axios
+          .get(
             `https://giftfairy-be-server.onrender.com/api/filter/response/${user.email}/`
           )
           .then((response) => {
             const items = response.data;
-            setProfile(items);
+            setUserProfileHistory(items);
           });
       } else {
         // No user is signed in
         setAuthCurrentUser(null);
+        setUserFirstName([]);
       }
     });
-
     return () => unsubscribe(); // Cleanup function to unsubscribe from the observer
   }, []);
 
   console.log(authCurrentUser);
-  
+
   return (
     <>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 40 1440 290">
         <path
           fill="#ffa517"
-          fill-opacity=".75"
+          fillOpacity=".75"
           d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
         ></path>
       </svg>
@@ -64,7 +73,8 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
         <NavBar />
       </div>
       <div className="userProfileContainer">
-        <h1>User Profile</h1>
+        {(authCurrentUser && userFirstName.length > 0) && <h1>{userFirstName[0].first_name}'s Profile</h1>}
+        {!authCurrentUser && <h1>User Profile</h1>}
 
         {!authCurrentUser && (
           <>
@@ -79,7 +89,7 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
           <>
             <h2>Selection History</h2>
             <div className="userProfileHistory">
-              {profile
+              {userProfileHistory
                 .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
                 .map((item, index) => (
                   <div key={index} className="userHistoryByDate">
