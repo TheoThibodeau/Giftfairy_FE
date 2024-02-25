dc33ab638581c954fbf79cee9de95874fb5b9d54
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-24 23:09:52 -0500

    cleaned up codebase, took out unused lines of code and other inefficiencies take 22
    Co-Authored-by: Christian Dezha <dezhac6@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   const handleUserLogin = (newUser) => {
     setUser(newUser);
-  };
+  };    
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
