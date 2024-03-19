3822bb7f45d46e00ad50bdb676f2a3fddc54ff80
kekedezha <dezhac6@gmail.com>
2024-03-19 18:27:20 -0400

    deleted filters2.json from dir, added comments/doc to landing, productCarousel and typewriter files
    Co-authored-by: Theodore Thibodeau <teddythibodeau@gmail.com>

--- src/components/productCarousel.jsx
+++ src/components/productCarousel.jsx
@@ -4,14 +4,20 @@ import Amazon from "../images/amazon.png";
 const ProductCarousel = ({ itemTitle, itemDescrip }) => {
   return (
     <>
+      {/* Div to contain product carousel - javascript/bootstrap carousel */}
       <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" data-interval="10000">
+        {/* Ordered list for carousel indicators */}
         <ol className="carousel-indicators" style={{ paddingBottom: "25px", borderRadius: "1px" }}>
+          {/* Map through itemTitle array to generate proper number of list item indicators for items */}
+          {/* If index === 0 then assign it a class name of 'active' to initialize first product/carousel */}
           {itemTitle.map((title, index) => (
             <li data-target="#carouselExampleIndicators" data-slide-to={index} className={index === 0 ? 'active': ''}>
             </li>
           ))}
         </ol>
         <div className="carousel-inner">
+          {/* Map through itemTitle array to generate cards for product carousel */}
+          {/* If index === 0 then assign it a class name of 'active' to initialize first product/carousel */}
           {itemTitle.map((title, index) => (
             <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
              <img className="productImage" src={Amazon} alt={`Slide ${index + 1}`} />
@@ -24,7 +30,7 @@ const ProductCarousel = ({ itemTitle, itemDescrip }) => {
               </div>
               <a
                 href={`https://www.amazon.com/s?k=${encodeURIComponent(title)}
-                &tag=${"giftfairy0c9-20"}`}
+                &tag=${"giftfairy0c9-20"}`} /* Amazon Affiliate id*/
                 target="_blank"
                 rel="noreferrer"
               >
@@ -33,10 +39,12 @@ const ProductCarousel = ({ itemTitle, itemDescrip }) => {
            </div>
           ))}
         </div>
+        {/* Left arrow to traverse through product carousel back to previous item */}
         <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
           <span className="carousel-control-prev-icon"aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
         </a>
+        {/* Right arrow to traverse through product carousel forward to next item */}
         <a className="carousel-control-next" href="#carouselExampleIndicators" role="button"data-slide="next">
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="sr-only">Next</span>
--- src/components/typewriter.jsx
+++ src/components/typewriter.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect} from 'react';
 
+// TypeWriter function to be used throughout app to give the effect of the 'gift fairy' talking to the user
+// Param(text) - String passed in to be iterated one character at a time
 export default function TypeWriter ({text}) {
+    // displayText - state variable to hold what will be printed out/returned by function out to UI
     const [displayText, setDisplayText] = useState(" ");
 
     useEffect(() => {
         let i = -1;
-        console.log(text);
         const typingInterval = setInterval(() => {
+            // iterate through 'text' and append it to displayText
             if (i < text.length) {
               setDisplayText((prev) => prev + text.charAt(i));
               i++;
-            } else {
+            } else {  //If iteration has reached the end of the string then clear the interval
               clearInterval(typingInterval);
             }
           }, 51);
-      
+          
+          // For the return, clear interval and clear DisplayText state variable to so that next string is ready to be received.
           return () => {
             clearInterval(typingInterval);
             setDisplayText("")
