09845555b5321548fd6ca1c3d305a87cf7ecb3cc
kekedezha <dezhac6@gmail.com>
2024-03-20 17:08:47 -0400

    separated arrow svgs into own components to render to userProfile component.
    Co-authored-by: Theodore Thibodeau <teddythibodeau@gmail.com>

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
 import NavBar from "./navbar";
+import UpArrows from "./upArrows.jsx";
+import DownArrows from "./downArrows.jsx"
 import UserAuthentication from "./userAuthentication.jsx";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
@@ -17,7 +19,6 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
   const handleDateClick = (index) => {
     setSelectedDate(selectedDate === index ? null : index);
   };
-
 
   // Handler function to expand selections inside of card
   // Toggle selectedItem state variable to expand or minimize details
@@ -110,18 +111,12 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
                       <h4>Recipient: {item.giftee_name}</h4>
                       {selectedDate == index ? 
                       (
-                        <svg id="more-arrows" viewBox="0 0 125 115" onClick={() => handleDateClick(index)}>
-                          <polygon className="arrow-top" points="37.6,1.3 1.8,27.9 3.3,29.2 37.6,3.9 71.9,29.2 73.7,27.9"/>
-                          <polygon className="arrow-middle" points="37.6,18.7 0.8,45.8 4.4,48.1 37.6,23.2 71.2,48.1 74.5,45.8"/>
-                          <polygon className="arrow-bottom" points="37.6,36.1 0,64 5.1,67.3 37.6,43.2 70.4,67.3 75.5,64"/>
-                        </svg>
-                      ) : (
-                        <svg id="more-arrows" viewBox="0 0 125 115" onClick={() => handleDateClick(index)}>
-                          <polygon className="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
-                          <polygon className="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
-                          <polygon className="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
-                        </svg>
-                          )
+                        <UpArrows onClickHandler={handleDateClick} index={index} />
+                      ) 
+                        : 
+                      (
+                        <DownArrows onClickHandler={handleDateClick} index={index} />
+                      )
                       }
                     </div>
                     {selectedDate == index && (
@@ -134,18 +129,12 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
                           <h4>Selections:</h4>
                           {selectedItem == index ? 
                             (
-                            <svg id="more-arrows" viewBox="0 0 125 115" onClick={() => handleSelectionsClick(index)}>
-                              <polygon className="arrow-top" points="37.6,1.3 1.8,27.9 3.3,29.2 37.6,3.9 71.9,29.2 73.7,27.9"/>
-                              <polygon className="arrow-middle" points="37.6,18.7 0.8,45.8 4.4,48.1 37.6,23.2 71.2,48.1 74.5,45.8"/>
-                              <polygon className="arrow-bottom" points="37.6,36.1 0,64 5.1,67.3 37.6,43.2 70.4,67.3 75.5,64"/>
-                            </svg>
-                            ) : (
-                            <svg id="more-arrows" viewBox="0 0 125 115" onClick={() => handleSelectionsClick(index)}>
-                              <polygon className="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
-                              <polygon className="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
-                              <polygon className="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
-                            </svg>
-                                )
+                              <UpArrows onClickHandler={handleSelectionsClick} index={index} />
+                            ) 
+                              : 
+                            (
+                              <DownArrows onClickHandler={handleSelectionsClick} index={index} />
+                            )
                           }
                         </div>
                         {selectedItem === index && (
