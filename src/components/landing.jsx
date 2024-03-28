e7237ed324f10fe56134ea7736b16a0b34b4f52d
Theodore Thibodeau <teddythibodeau@gmail.com>
2024-03-28 16:46:02 -0400

    orangeSwoop!
    Co-Authored-By: Christian Dezha <dezhac6@gmail.com>

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -5,15 +5,13 @@ import RightArrow from "../images/right.png";
 import InfoIcon from "../images/info.png";
 import Amazon from "../images/amazon1.png";
 import Popup from "reactjs-popup";
+import OrangeSwoop from "./Headers and Footers/orangeSwoop";
 
 const Landing = () => {
   return (
     <>
-      {/* Orange back-splash running across top of screen for all pages. */}
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 80 1440 200">
-        <path fill="#ffa517" fillOpacity="1" d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z">
-         </path>
-      </svg>
+      <OrangeSwoop fillOpacity={"1"}/>
+
       {/* Popup on landing page that indicates app is an Amazon Associate */}
       <Popup trigger={<img className="infoIconImg" style={{ zIndex: 4 }} src={InfoIcon} height={30} width={30}></img>} 
       arrow={false} position="right right">
--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -5,6 +5,7 @@ import NavBar from "./navbar";
 import Arrows from './arrows.jsx';
 import UserAuthentication from "./userAuthentication.jsx";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import OrangeSwoop from "./Headers and Footers/orangeSwoop.jsx";
 
 const GetProfileResponse = ({ handleUserLogin, authentication }) => {
   const [userProfileHistory, setUserProfileHistory] = useState([]);
@@ -65,10 +66,8 @@ const GetProfileResponse = ({ handleUserLogin, authentication }) => {
   // User Profile Page UI return
   return (
     <>
-      {/* Back-splash across all pages */}
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 40 1440 290">
-        <path fill="#ffa517" fillOpacity=".75" d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,96C800,96,880,160,960,165.3C1040,171,1120,117,1200,85.3C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
-      </svg>
+      <OrangeSwoop fillOpacity={".75"}/>
+
 
       {/* Navigation bar that includes home button, app title, and profile button */}
       <div className="navbar-ProgressBar-Container" style={{ height: "81px" }}>
