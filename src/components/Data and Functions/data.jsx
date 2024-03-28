8282128ce92979b4bd2c29258a5a430218dbd3ae
Theodore Thibodeau <teddythibodeau@gmail.com>
2024-03-28 16:07:01 -0400

    created data.jsx to store various objects and functions used in filters.jsx
    Co-Authored-By: Christian Dezha <dezhac6@gmail.com>

---
+++ src/components/Data and Functions/data.jsx
@@ -0,0 +1,32 @@
+const progressValues = {
+    gifteeName: 0,
+    gender: 9,
+    age: 19,
+    relationship: 30,
+    priceRange: 38,
+    occasion: 46,
+    giftType: 54,
+    interests: 63,
+    activity: 72,
+    personality: 81,
+    nature: 90,
+    generate: 100,
+  };
+
+  const promptMessages = {
+    gifteeName:
+      "Beefore we get started, I'm curious, what is your giftee's name?",
+    gender: "What is their gender?",
+    age: "How old are they?",
+    relationship: "What is your relationship with them?",
+    priceRange: "What is your price range?",
+    occasion: "What's the occasion?",
+    giftType: "What gifts would they be interested in?",
+    interests: "What are their interests?",
+    activity: "What is their activity level?",
+    personality: "What is their personality type?",
+    nature: "Do they prefer being inside or outside?",
+    generate: "",
+  };
+  
+  export { progressValues, promptMessages };
--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -9,6 +9,7 @@ import TypeWriter from "./typewriter";
 import UserAuthentication from "./userAuthentication";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import ProductCarousel from "./productCarousel";
+import { progressValues, promptMessages } from "./Data and Functions/data";
 
 const Filters = ({ handleUserLogin, authentication }) => {
   const [age, setAge] = useState("");
@@ -179,37 +180,22 @@ const Filters = ({ handleUserLogin, authentication }) => {
     setGenerate(selectedGenerate);
   };
 
-  const progressValues = {
-    gifteeName: 0,
-    gender: 9,
-    age: 19,
-    relationship: 30,
-    priceRange: 38,
-    occasion: 46,
-    giftType: 54,
-    interests: 63,
-    activity: 72,
-    personality: 81,
-    nature: 90,
-    generate: 100,
-  };
-
   //Object containing messages for each selection page
-  const promptMessages = {
-    gifteeName:
-      "Beefore we get started, I'm curious, what is your giftee's name?",
-    gender: "What is their gender?",
-    age: "How old are they?",
-    relationship: "What is your relationship with them?",
-    priceRange: "What is your price range?",
-    occasion: "What's the occasion?",
-    giftType: "What gifts would they be interested in?",
-    interests: "What are their interests?",
-    activity: "What is their activity level?",
-    personality: "What is their personality type?",
-    nature: "Do they prefer being inside or outside?",
-    generate: "",
-  };
+  // const promptMessages = {
+  //   gifteeName:
+  //     "Beefore we get started, I'm curious, what is your giftee's name?",
+  //   gender: "What is their gender?",
+  //   age: "How old are they?",
+  //   relationship: "What is your relationship with them?",
+  //   priceRange: "What is your price range?",
+  //   occasion: "What's the occasion?",
+  //   giftType: "What gifts would they be interested in?",
+  //   interests: "What are their interests?",
+  //   activity: "What is their activity level?",
+  //   personality: "What is their personality type?",
+  //   nature: "Do they prefer being inside or outside?",
+  //   generate: "",
+  // };
 
   const handleStateSet = (key, value) => {
     if (key === "Giftee Name") {
