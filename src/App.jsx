a8eba3c7721ec22b0dc1eb0ef8927d7979ba109e
kekedezha <dezhac6@gmail.com>
2024-04-04 15:51:50 -0400

    added .env file in root dir for firebase config
    Co-authored-by: Theodore Thibodeau <teddythibodeau@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GetProfileResponse from "./components/userProfile";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+require('dotenv').config();
 
 function App() {
   const [user, setUser] = useState("");
@@ -17,14 +18,15 @@ function App() {
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyD31E0qyRff38YbacqfmJX1mLePHxanrws",
-    authDomain: "giftfairy-a84c0.firebaseapp.com",
-    projectId: "giftfairy-a84c0",
-    storageBucket: "giftfairy-a84c0.appspot.com",
-    messagingSenderId: "360717671227",
-    appId: "1:360717671227:web:1a35bb12a01c2799f7f51e",
-    measurementId: "G-TDHVENEFD9",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   };
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
